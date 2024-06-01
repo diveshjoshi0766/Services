99ff82cdb4e3c47631713e02150ac64fd30b2711
divesh_joshi <diveshjoshi0766@gmail.com>
2024-06-01 15:38:56 +0530

    CORS fixed

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
+app.use(cors()); // Add this line to enable CORS
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
